4c620d00ddbd7281d9c691c56b8a8fe0b563be2b
黃郁婷 <kaze1010719@gmail.com>
2018-10-07 00:44:42 +0800

    while i > 0 version

--- password.py
+++ password.py
@@ -1,12 +1,14 @@
 password = 'a123456'
 i = 3
-while True:
+while i > 0:
+	i -= 1
 	pWord = input('請輸入密碼: ')
 	if pWord == 'a123456':
 		print('登入成功!') 
 		break
 	else:
-		i -= 1
-		if i == 0:
-			break
-		print('密碼錯誤! 還有', i, '次機會')
+		
+		if i > 0:			
+			print('密碼錯誤! 還有', i, '次機會')
+		else:
+			print('密碼錯誤! 沒機會嘗試了! 要鎖帳號了啦!')
